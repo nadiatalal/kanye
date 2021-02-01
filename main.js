a5345c47ac7b021a8f8e5559a2bddcbb3daf2065
nadia talal <talal-nadia@hotmail.com>
2021-02-01 15:12:06 +0100

    ajax function onload

---
+++ main.js
@@ -0,0 +1,16 @@
+//mes selecteurs
+var quote = document.getElementById('quote');
+
+
+//mes variables
+
+var citation = "";
+
+//mes evenements
+
+    window.onload = async() => {
+        await fetch("https://api.kanye.rest/")
+        .then(reponse => reponse.json())
+        .then(data => citation = data.quote);
+        quote.textContent = citation;
+    }
